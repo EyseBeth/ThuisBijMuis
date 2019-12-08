01b34468234dc61862706a6278e42659c0108f1d
Adrian <adrian.vermeij@hva.nl>
2019-12-08 14:54:44 +0100

    First Page Done
    Added custom behaviour so object renderers are switched on and off when CustomBehaviour is called. also added function to ActivateInteractbles is called when an object is clicked on. Changed hitbox of page 2.

---
+++ Assets/_Scripts/Clickables/CustomBehaviour/ChangeObjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThuisBijMuis.Games.Interactables.CustomBehaviours
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ChangeObjects : MonoBehaviour, IClickable
+    {
+        public GameObject[] firstSet;
+        public GameObject[] secondSet;
+
+        public bool objectsChanged = false;
+        
+        void Start()
+        {
+            //ensures the first set is active and the second is inactive on startup.
+            ChangeThem(true, false);
+        }
+
+        public void ExecuteCustomBehaviour()
+        {
+            //Checks if the objects have already been switched, if it hasnt, it does so.
+            if (objectsChanged == false)
+            {
+                objectsChanged = true;
+
+                ChangeThem(false, true);
+            }
+        }
+
+        private void ChangeThem(bool forFirst, bool forSec)
+        {
+            foreach (GameObject obj in firstSet)
+            {
+                obj.GetComponent<SpriteRenderer>().enabled = forFirst;
+            }
+
+            foreach (GameObject obj in secondSet)
+            {
+                obj.GetComponent<SpriteRenderer>().enabled = forSec;
+            }
+        }
+    }
+}
