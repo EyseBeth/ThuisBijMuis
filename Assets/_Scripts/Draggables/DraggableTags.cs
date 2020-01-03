f63bb47a8eed6e815ba1f4feb0abfae10a9d6226
Jessica Nguyen <jessica.nguyen@hva.nl>
2020-01-03 16:53:32 +0100

    Merge conflicts

--- Assets/_Scripts/Draggables/DraggableTags.cs
+++ Assets/_Scripts/Draggables/DraggableTags.cs
@@ -7,6 +7,14 @@ namespace ThuisBijMuis.Games.Interactables {
     //The tags used to check for acceptable drop positions for draggables are stored here
     public enum DroppableTags {
         Jacket,
-        Pants
+        Pants,
+        Apple,
+        Bread,
+        BlueSock,
+        RedSock,
+        YellowSock,
+        GreenSock,
+        DarkRedSock,
+        DarkBlueSock
     }
 }
