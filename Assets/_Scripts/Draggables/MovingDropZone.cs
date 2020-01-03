d0b54c733edcc459d37a55b8e82d0fe889df82fd
Jessica Nguyen <jessica.nguyen@hva.nl>
2020-01-03 16:11:57 +0100

    Attempting to solve a merge conflict with DraggableItem.cs and DropZone.cs scripts when pulling changes from the SceneAssembling branch needed for the animations

---
+++ Assets/_Scripts/Draggables/MovingDropZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThuisBijMuis.Games.Interactables
+{
+    public class MovingDropZone : DropZone
+    {
+        public GameObject target;
+        private float step = 0.1f;
+
+        void Update()
+        {
+            if (!IsDropped) 
+                return;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step*Time.deltaTime);
+            
+        }
+    }
+}
---
+++ Assets/_Scripts/Draggables/VanishDraggable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThuisBijMuis.Games.Interactables
+{
+    public class VanishDraggable : DraggableItem
+    {
+        public override void OnMouseUp()
+        {
+            if (currentDropZone != null && currentDropZone.CheckTags(itemTags))
+            {
+                Drop(currentDropZone);
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<BoxCollider>().enabled = false;
+            }
+            else Return();
+            currentDropZone = null;
+            selected = false;
+        }
+    }
+}
