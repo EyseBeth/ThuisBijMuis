64aff5e650cc4760b50764b5af0db699d08b7402
SemihOrhan <semih99orhan@gmail.com>
2020-01-07 12:47:29 +0100

    Same update for the release of draggables.
    Created DraggableAudio which inherits from DraggableItem and overrides the Release method.

---
+++ Assets/_Scripts/Draggables/Custom Draggables/DraggableAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThuisBijMuis.Games.Interactables.CustomBehaviours
+{
+#pragma warning disable 0649
+    [RequireComponent(typeof(AudioSource))]
+    public class DraggableAudio : DraggableItem
+    {
+        [SerializeField] private AudioClipObject clip;
+
+        private AudioSource audioSource;
+
+        private void Start() => audioSource = GetComponent<AudioSource>();
+
+        public override void Release()
+        {
+            if (currentDropZone != null && currentDropZone.CheckTags(ItemTags))
+            {
+                Drop(currentDropZone);
+                PlayAudio();
+            }
+            else Return();
+            currentDropZone = null;
+            selected = false;
+        }
+
+        private void PlayAudio()
+        {
+            if (!audioSource.isPlaying && clip)
+                audioSource.PlayOneShot(clip.audioClip[clip.counter++]);
+
+            if (clip.counter >= clip.audioClip.Length)
+                clip.counter = clip.audioClip.Length - 1;
+        }
+    }
+}
