859b0b4805d04dc7ea2fa8564ecd234edc1c5851
Cornetti <EyseBeth@gmail.com>
2020-01-15 14:39:54 +0100

    made reload script and relaod button at last page

---
+++ Assets/_Scripts/SceneManagment/SceneReloader.cs
@@ -0,0 +1,13 @@
+﻿using ThuisBijMuis.Games.PageSliding;
+using ThuisBijMuis.Timers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloader : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
